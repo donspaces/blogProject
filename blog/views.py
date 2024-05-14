bec92407c668f5cd2500375fccade15d819a9a7a
donspaces <aicder93@gmail.com>
2024-05-14 02:17:11 -0600

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
