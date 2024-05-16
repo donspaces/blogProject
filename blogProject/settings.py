062910de7fcc603e66b2a4cbbf55f80e0c476055
donspaces <aicder93@gmail.com>
2024-05-15 18:38:57 -0600

    update commit

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@&b!s&w)q&zzitb1lnb)brmkj@6ijr1ie++3&mbwbc09awba_m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog.donspaces.com']
 
 
 # Application definition
